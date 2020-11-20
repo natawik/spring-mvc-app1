3250fb625181ba29223d6ae45eef267a9c58f411
natawik <natawik.spb@gmail.com>
2020-11-20 09:56:02 +0300

    configuration with xml-files

--- src/main/java/ru/klabukova/springcourse/HelloController.java
+++ src/main/java/ru/klabukova/springcourse/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
+	//configuration with xml-files
 
     @GetMapping("/hello-world")
     public String sayHello() {
